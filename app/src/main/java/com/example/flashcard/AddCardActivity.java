c709e7f46a61c52063bb39831e819b20501aa285
RuiminGao <rpg5384@psu.edu>
2021-02-24 21:45:04 +0800

    update AddCardActivity: edit options

--- app/src/main/java/com/example/flashcard/AddCardActivity.java
+++ app/src/main/java/com/example/flashcard/AddCardActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import static com.example.flashcard.MainActivity.KEY_CARD_OPTIONS;
 
 public class AddCardActivity extends AppCompatActivity {
 
     ImageView ivCancel, ivSave;
     EditText etQuestion,etAnswer;
+    EditText[] etOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,13 @@ public class AddCardActivity extends AppCompatActivity {
         ivSave = findViewById(R.id.ivSave);
         etQuestion = findViewById(R.id.etQuestion);
         etAnswer = findViewById(R.id.etAnswer);
+        etOptions = new EditText[]{findViewById(R.id.etOption1), findViewById(R.id.etOption2), findViewById(R.id.etOption3)};
 
         etQuestion.setText(getIntent().getStringExtra(MainActivity.KEY_CARD_QUESTION));
         etAnswer.setText(getIntent().getStringExtra(MainActivity.KEY_CARD_ANSWER));
+        for(int j = 0; j < 3; j++) {
+            etOptions[j].setText(getIntent().getStringExtra(MainActivity.KEY_CARD_OPTIONS[j]));
+        }
 
         ivCancel.setOnClickListener(view -> {
             Intent intent = new Intent();
@@ -32,11 +40,18 @@ public class AddCardActivity extends AppCompatActivity {
         });
 
         ivSave.setOnClickListener(view -> {
-            Intent intent = new Intent();
-            intent.putExtra(MainActivity.KEY_CARD_QUESTION, etQuestion.getText().toString());
-            intent.putExtra(MainActivity.KEY_CARD_ANSWER, etAnswer.getText().toString());
-            setResult(RESULT_OK, intent);
-            finish();
+            if (etQuestion.getText().toString().isEmpty() || etAnswer.getText().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Please enter your question and answer.", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.KEY_CARD_QUESTION, etQuestion.getText().toString());
+                intent.putExtra(MainActivity.KEY_CARD_ANSWER, etAnswer.getText().toString());
+                for(int j = 0; j < etOptions.length; j++) {
+                    intent.putExtra(MainActivity.KEY_CARD_OPTIONS[j], etOptions[j].getText().toString());
+                }
+                setResult(RESULT_OK, intent);
+                finish();
+            }
         });
 
     }
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.flashcard;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String KEY_CARD_QUESTION = "card_question";
     public static final String KEY_CARD_ANSWER = "card_answer";
+    public static final String[] KEY_CARD_OPTIONS = {"option_1","option_2","option_3"};
 
     ImageView ivVisible, ivInvisible, ivEdit, ivAdd;
     TextView tvQuestion, tvAns;
@@ -41,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(MainActivity.this, AddCardActivity.class);
             i.putExtra(KEY_CARD_QUESTION, tvQuestion.getText().toString());
             i.putExtra(KEY_CARD_ANSWER, tvAns.getText().toString());
+            for(int j = 0; j < tvOptions.length; j++) {
+                i.putExtra(KEY_CARD_OPTIONS[j], tvOptions[j].getText().toString());
+            }
             startActivityForResult(i, ADD_CARD_CODE);
         });
 
@@ -51,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
         rlCard.setOnClickListener(view -> {
             for (TextView tvOption : tvOptions) {
-                tvOption.setBackgroundColor(getResources().getColor(R.color.grey));
-                tvOption.setTextColor(getResources().getColor(R.color.grey2));
+                tvOption.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.grey));
+                tvOption.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.grey2));
             }
         });
 
@@ -73,13 +79,13 @@ public class MainActivity extends AppCompatActivity {
         for (TextView tvOption : tvOptions) {
             tvOption.setOnClickListener(view -> {
                 if (view == tvOptions[answerKey]) {
-                    view.setBackgroundColor(getResources().getColor(R.color.green));
-                    ((TextView) view).setTextColor(getResources().getColor(R.color.green2));
+                    view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.green));
+                    ((TextView) view).setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.green2));
                 } else {
-                    tvOptions[answerKey].setBackgroundColor(getResources().getColor(R.color.green));
-                    tvOptions[answerKey].setTextColor(getResources().getColor(R.color.green2));
-                    view.setBackgroundColor(getResources().getColor(R.color.red));
-                    ((TextView) view).setTextColor(getResources().getColor(R.color.red2));
+                    tvOptions[answerKey].setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.green));
+                    tvOptions[answerKey].setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.green2));
+                    view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.red));
+                    ((TextView) view).setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.red2));
                 }
 
             });
@@ -91,8 +97,17 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK) {
             assert data != null;
+            Toast.makeText(getApplicationContext(), "Saved.", Toast.LENGTH_SHORT).show();
             tvQuestion.setText(data.getStringExtra(KEY_CARD_QUESTION));
             tvAns.setText(data.getStringExtra(KEY_CARD_ANSWER));
+            for(int j = 0; j < 3; j++) {
+                String text = data.getStringExtra(KEY_CARD_OPTIONS[j]);
+                if (text.isEmpty()) {
+                    tvOptions[j].setText("NULL");
+                } else {
+                    tvOptions[j].setText(text);
+                }
+            }
         }
     }
 
